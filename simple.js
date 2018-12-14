1de5f0bfc6c5dced605d9acad04885b3b72983dd
Peter Leonov <gojpeg@gmail.com>
2018-12-14 20:37:01 +0100

    add some errors

--- simple.js
+++ simple.js
@@ -13,15 +13,15 @@ const getState2 = state => state.state2;
 
 // component
 
-type OwnProps = {|
-  own1: Date,
-  // [string]: mixed,
-|};
+// type OwnProps = {|
+//   own1: Date,
+//   // [string]: mixed,
+// |};
 
-type StateProps = {|
-  state1: string,
-  state2: number,
-|};
+// type StateProps = {|
+//   state1: string,
+//   state2: number,
+// |};
 
 type Props = {
   own1: string,
@@ -32,17 +32,22 @@ type Props = {
 class WC extends Component<Props, {}> {
   render() {
     const { own1, state1, state2, ...rest } = this.props;
-    // this.props.own1.toString();
+    this.props.own1.toString();
     return null;
   }
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  state1: getState1(state).concat(ownProps.own1),
-  state2: getState2(state),
-  state3: getState2(state),
-});
+const mapStateToProps = (state, ownProps) => {
+  // // $FlowFixMe state1 is missing in Props
+  // ownProps.state1;
+  return {
+    state1: getState1(state).concat(ownProps.own1),
+    state2: getState2(state),
+  };
+};
 
 export const C = connect<Props, State, _>(mapStateToProps)(WC);
 
-const render = <C own1="" nonExisting="sdsd" />;
+<C own1="" nonExisting="sdsd" />;
+// // $FlowFixMe missing own1 exists in Props
+// <C nonExisting="sdsd" />;
